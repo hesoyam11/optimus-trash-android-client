76dda318c09b4162d2747ffbf3499e3e26e91f2c
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-06-02 18:36:20 +0300

    implemented `LoginActivity`

--- app/src/main/java/com/optimustrash/androidclient/LoginActivity.kt
+++ app/src/main/java/com/optimustrash/androidclient/LoginActivity.kt
@@ -1,12 +1,42 @@
 package com.optimustrash.androidclient
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.gson.Gson
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class LoginActivity : AppCompatActivity() {
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.login_button)
+        loginButton.setOnClickListener { _ ->
+            val username = findViewById<EditText>(R.id.username).text.toString()
+            val password = findViewById<EditText>(R.id.password).text.toString()
+
+            val o = postLogin(username, password)
+                .map { Gson().fromJson(it, AccessToken::class.java) }
+                .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+
+            val loginErrorView = findViewById<TextView>(R.id.login_err_msg)
+            o.subscribe({
+                accessToken = it.access
+                loginErrorView.text = "Success!"
+            }, {
+                loginErrorView.text = it.message
+            })
+        }
     }
 }
+
+class AccessToken(
+    val access: String
+)
---
+++ app/src/main/java/com/optimustrash/androidclient/Utils.kt
@@ -0,0 +1,49 @@
+package com.optimustrash.androidclient
+
+import io.reactivex.Observable
+import java.io.DataOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
+const val domain = "http://192.168.0.102:8000"
+var accessToken = ""
+
+fun postLogin(username: String, password: String) = Observable.create<String> { it ->
+
+    val requestBody = "{\"username\":\"$username\", \"password\":\"$password\"}"
+    val postData = requestBody.toByteArray(Charsets.UTF_8)
+
+    val urlConnection = URL("$domain/api/token/").openConnection() as HttpURLConnection
+    urlConnection.doOutput = true
+    urlConnection.instanceFollowRedirects = false
+    urlConnection.requestMethod = "POST"
+    urlConnection.setRequestProperty(
+        "Content-Type", "application/json"
+    )
+    urlConnection.setRequestProperty(
+        "charset", "utf-8"
+    )
+    urlConnection.setRequestProperty(
+        "Content-Length", Integer.toString(postData.size)
+    )
+    urlConnection.useCaches = false
+
+    val wr = DataOutputStream(urlConnection.outputStream)
+    wr.use {
+        it.write(postData)
+    }
+
+    try {
+        urlConnection.connect()
+
+        if (urlConnection.responseCode != HttpURLConnection.HTTP_OK)
+            it.onError(RuntimeException(urlConnection.responseMessage))
+        else {
+            val str = urlConnection.inputStream.bufferedReader().readText()
+            it.onNext(str)
+        }
+    }
+    finally {
+        urlConnection.disconnect()
+    }
+}
