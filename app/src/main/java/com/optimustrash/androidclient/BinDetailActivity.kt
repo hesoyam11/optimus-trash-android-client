a45db4702cf8aca65b9f02a0ce38707ac41434ab
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-06-03 21:22:08 +0300

    a bin's unique token retrieving on appropriate button click

--- app/src/main/java/com/optimustrash/androidclient/BinDetailActivity.kt
+++ app/src/main/java/com/optimustrash/androidclient/BinDetailActivity.kt
@@ -3,6 +3,7 @@ package com.optimustrash.androidclient
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import android.widget.TextView
 import com.google.gson.Gson
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -18,6 +19,8 @@ class BinDetailActivity : AppCompatActivity() {
     private lateinit var currentWeightTextView: TextView
     private lateinit var maxWeightTextView: TextView
     private lateinit var fullnessTextView: TextView
+    private lateinit var showTokenButton: Button
+    private lateinit var tokenTextView: TextView
 
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,21 +34,37 @@ class BinDetailActivity : AppCompatActivity() {
         currentWeightTextView = findViewById(R.id.current_weight)
         maxWeightTextView = findViewById(R.id.max_weight)
         fullnessTextView = findViewById(R.id.fullness)
+        showTokenButton = findViewById(R.id.show_token_button)
+        tokenTextView = findViewById(R.id.token)
 
         binId = intent.getStringExtra("binId").toInt()
         val o = getBinDetail()
             .map{ Gson().fromJson(it, BinItem::class.java)}
             .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
 
-        o.subscribe({
+        o.subscribe({ it ->
             binIdTextView.text = "${binIdTextView.text}: ${it.id}"
             longitudeTextView.text = "${longitudeTextView.text}: ${it.longitude}"
             latitudeTextView.text = "${latitudeTextView.text}: ${it.latitude}"
             currentWeightTextView.text = "${currentWeightTextView.text}: ${it.currentWeight}"
             maxWeightTextView.text = "${maxWeightTextView.text}: ${it.maxWeight}"
             fullnessTextView.text = "${fullnessTextView.text}: ${it.fullness}"
+            showTokenButton.setOnClickListener { _ ->
+                val ob = getBinToken()
+                    .map{ Gson().fromJson(it, BinToken::class.java)}
+                    .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+                ob.subscribe({
+                    tokenTextView.text = it.token
+                }, {
+                    errorTextView.text = it.message
+                })
+            }
         }, {
             errorTextView.text = it.message
         })
     }
 }
+
+class BinToken(
+    val token: String
+)
--- app/src/main/java/com/optimustrash/androidclient/Utils.kt
+++ app/src/main/java/com/optimustrash/androidclient/Utils.kt
@@ -94,3 +94,22 @@ fun getBinDetail() = Observable.create<String> {
         urlConnection.disconnect()
     }
 }
+
+fun getBinToken() = Observable.create<String> {
+    val urlConnection = URL("$domain/api/bins/$binId/token/").openConnection() as HttpURLConnection
+    urlConnection.setRequestProperty(
+        "Authorization", "Bearer $accessToken"
+    )
+    try {
+        urlConnection.connect()
+        if (urlConnection.responseCode != HttpURLConnection.HTTP_OK)
+            it.onError(RuntimeException(urlConnection.responseMessage))
+        else {
+            val str = urlConnection.inputStream.bufferedReader().readText()
+            it.onNext(str)
+        }
+    }
+    finally {
+        urlConnection.disconnect()
+    }
+}
